873f2aab6f23095f7598e20dcc360f7a43ca44bd
Dan Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
2021-09-02 20:11:09 -0400

    Added nodemon and get root route

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// yay!
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the machine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App started at http://localhost:${PORT}/`);
+});
