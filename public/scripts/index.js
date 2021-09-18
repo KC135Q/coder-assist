6e2c11715df583bc2aa57dbe50c6807ae3779e6f
Dan Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
2021-09-18 11:28:30 -0400

    profile and home page with script

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,20 @@
 console.log("This works and I'm a script :)");
+
+// On sign in, ....
+// - get the data from the form
+const signin = (event) => {
+  event.preventDefault();
+  // alert("clicked");
+  // - validate the form information --- username was entered, correct length, password was entered, correct characters and length
+
+  // - authenticate the user --- check the database for the user
+  // POST to api ... /api/v1/user
+    // Stay tuned... next time we will talk about CRUD and POST GET DELETE PUT ...
+
+  // - redirect to an account/profile page if found, otherwise "Matching username/password not found."
+  // Simulate an HTTP redirect:
+  window.location.replace("/profile");
+};
+
+// add an event listener to the signin-btn
+document.querySelector("#signin-btn").addEventListener("click", signin);
--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/profile", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/profile.html"));
 });
 
 app.listen(PORT, () => {
